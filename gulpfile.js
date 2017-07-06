66ef3d2dd875757a41057218cd277006c079e543
Rajpurkar <pranav.rajpurkar@gmail.com>
2017-07-06 02:41:22 -0700

    Add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp')
 var pug = require('gulp-pug')
+var watch = require('gulp-watch')
+var browserSync = require('browser-sync').create()
 
 gulp.task('index', function buildHTML () {
   return gulp.src('views/index.pug')
-  .pipe(pug({}))
+  .pipe(pug({locals: {index: true}}))
   .pipe(gulp.dest('.'))
 })
 
@@ -13,4 +15,18 @@ gulp.task('projects', function buildHTML () {
   .pipe(gulp.dest('./projects/'))
 })
 
-gulp.task('default', ['index', 'projects'])
+gulp.task('build', ['index', 'projects'])
+
+gulp.task('watch_build', ['build'], function () {
+  return gulp.watch('./views/**/*.pug', ['build', browserSync.reload])
+})
+
+gulp.task('browser-sync', ['build'], function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  })
+})
+
+gulp.task('default', ['browser-sync', 'watch_build'])
